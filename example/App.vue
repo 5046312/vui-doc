d55c61b2fe43693a5ea20450e0bc25a70214049a
5046312 <5046312@qq.com>
2019-02-25 14:30:48 +0800

    first commit

---
+++ example/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {}
+};
+</script>
+
+<style lang='scss' scope>
+html,
+body{
+    height: 100%;
+    width: 100%;
+    overflow-x: auto;
+}
+#app{
+    height: 100%;
+    .main-contain{
+        padding: 0;
+    }
+}
+</style>
---
+++ example/views/components/layout.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-layout>
+        <div class="main-contain">
+            <v-demo :code="code" desc="基础说明部分">
+                
+            </v-demo>
+        </div>
+    </v-layout>
+</template>
+<script>
+import code from "@/codes/layout";
+export default {
+    components: {},
+    data() {
+        return {
+            code: code.layout
+        };
+    }
+};
+</script>
+<style lang="scss" scoped>
+</style>
